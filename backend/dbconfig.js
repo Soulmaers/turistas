07988451b6d3ddcfd0db2a052d395e3773b012af
Soulmaers <certiss@ya.ru>
2024-11-22 11:07:03 +0300

    бд

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -6,9 +6,11 @@ console.log(process.env.DATABASE)
 const config = {
     server: 'localhost', // Или '127.0.0.1', или 'localhost\имя_экземпляра'
     database: 'turistas', // Имя вашей базы данных
+    user: 'sa',
+    password: 'Alex',
     options: {
-        trustedConnection: true, // Используйте Windows Authentication
-        encrypt: false // Установите в true, если требуется шифрование
+        trustServerCertificate: true,
+        encrypt: false
     }
 };
 
